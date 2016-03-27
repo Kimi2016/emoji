1013e71f1efb7dab9ce0170115565916dec3618e
musicseli <345036769@qq.com>
2016-03-27 21:33:07 +0800

    加入对lua脚本的支持（基于ulua1.03），并参考六龙的对象缓存机制去放射

--- emoji/Assets/Game/Script/Main/Director.cs
+++ emoji/Assets/Game/Script/Main/Director.cs
@@ -36,6 +36,11 @@ __________#_______####_______####______________
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using LuaInterface;
+using System.Runtime.InteropServices;
+
+[Serializable] // 指示可序列化
+[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 1)] //平铺结构;Ansi字符;结构成员各按1字节对齐
 public class Director : MonoBehaviour {
 	private static Director mInstance;
 	public static Director GetInstance() {
@@ -45,6 +50,12 @@ public class Director : MonoBehaviour {
 		return mInstance;
 	}
 
+	private LuaState mLuaState;
+	public LuaState luaState {
+		get {
+			return mLuaState;
+		}
+	}
 	#region include some game manager
 	private Scheduler mScheduler;
 	public Scheduler scheduler {
@@ -65,15 +76,52 @@ public class Director : MonoBehaviour {
 		}
 	}
 	#endregion
-
-	void Start() {
+	void Awake() {
 		mInstance = this;
 		mScheduler = Scheduler.MakeInstance();
 		mUIManager = UIManager.MakeInstance();
 		mEventDispatcher = EventDispatcher.MakeInstance();
-		mUIManager.OpenView(EnumUIName.ChatView);
+		mLuaState = new LuaState();
+	}
+	void Start() {
+		Register(mLuaState.L);
+		mLuaState.DoString(@"
+			Director.GetInstance():OpenView(0)
+		");
 	}
 	void Update() { 
 		
+	}
+
+	public static void Register(IntPtr L) {
+		int oldTop = LuaDLL.lua_gettop(L);
+
+		LuaDLL.lua_newtable(L);
+		LuaDLL.lua_pushstdcallcfunction(L, new LuaCSFunction(Director.OpenView));
+		LuaDLL.lua_setfield(L, -2, "OpenView");
+		LuaDLL.lua_pushstdcallcfunction(L, new LuaCSFunction(Director.GetInstance));
+		LuaDLL.lua_setfield(L, -2, "GetInstance");
+		LuaDLL.lua_pushvalue(L, -1);
+		LuaDLL.lua_setfield(L, -2, "__index");
+		LuaDLL.lua_pushvalue(L, -1);
+		LuaDLL.lua_setglobal(L, "Director");
+
+		LuaDLL.lua_settop(L, oldTop);
+	}
+
+	[MonoPInvokeCallback(typeof(LuaCSFunction))]
+	public static int GetInstance(IntPtr L) {
+		int result = 1;
+		LuaStatic.addGameObject2Lua(L, GetInstance(), "Director");
+		return result;
+	}
+
+	[MonoPInvokeCallback(typeof(LuaCSFunction))]
+	public static int OpenView(IntPtr L) {
+		int result = 1;
+		Director director = LuaStatic.GetObj(L, 1) as Director;
+		EnumUIName uiName = (EnumUIName)LuaDLL.lua_tonumber(L, 2);
+		director.uiManager.OpenView(uiName);
+		return result;
 	}
 }
---
+++ emoji/Assets/uLua/Core/LuaStatic.cs
@@ -0,0 +1,219 @@
+namespace LuaInterface
+{
+	using System;
+	using System.IO;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Collections.Specialized;
+	using System.Reflection;
+	using System.Threading;
+    using System.Text;
+	using UnityEngine;
+	using System.Runtime.InteropServices;
+	
+	public class LuaStatic
+	{
+		public static readonly Dictionary<long, object> objs = new Dictionary<long, object>();
+		public static object GetObj(IntPtr luaState, int index) {
+			switch (LuaDLL.lua_type(luaState, index)) {
+				case LuaTypes.LUA_TNIL:
+					return null;
+
+				case LuaTypes.LUA_TBOOLEAN:
+					return LuaDLL.lua_toboolean(luaState, index);
+
+				case LuaTypes.LUA_TLIGHTUSERDATA:
+					return LuaDLL.lua_touserdata(luaState, index);
+
+				case LuaTypes.LUA_TNUMBER:
+					return LuaDLL.lua_tonumber(luaState, index);
+
+				case LuaTypes.LUA_TSTRING:
+					return LuaDLL.lua_tostring(luaState, index);
+
+				case LuaTypes.LUA_TUSERDATA: {
+						object obj2;
+						IntPtr ptr = LuaDLL.lua_touserdata(luaState, index);
+						if (!LuaStatic.objs.TryGetValue(ptr.ToInt64(), out obj2)) {
+							traceback(luaState, "GetObj: obj not exist");
+							LuaDLL.lua_error(luaState);
+							return null;
+						}
+						return obj2;
+					}
+			}
+			traceback(luaState, "GetObj: wrong value type: " + LuaDLL.luaL_typename(luaState, index));
+			LuaDLL.lua_error(luaState);
+			return null;
+		}
+		public static int addGameObject2Lua(IntPtr L, object obj, string metatable) {
+			if (obj == null) {
+				LuaDLL.lua_pushnil(L);
+				return 1;
+			}
+			if (objs.ContainsValue(obj)) {
+				return 1;
+			}
+			IntPtr ptr = LuaDLL.lua_newuserdata(L, 1);
+			Marshal.WriteByte(ptr, 1);
+			objs[ptr.ToInt64()] = obj;
+			LuaDLL.lua_getglobal(L, metatable);
+			if (LuaDLL.lua_isnil(L, -1)) {
+				LuaDLL.lua_pop(L, 1);
+			}
+			else {
+				LuaDLL.lua_setmetatable(L, -2);
+			}
+			return 1;
+		}
+		public static int traceback(IntPtr L, string err = "Lua traceback:") {
+			LuaDLL.lua_getglobal(L, "debug");
+			LuaDLL.lua_getfield(L, -1, "traceback");
+			LuaDLL.lua_remove(L, -2);
+			LuaDLL.lua_pushstring(L, err);
+			LuaDLL.lua_pushnumber(L, 1.0);
+			LuaDLL.lua_call(L, 2, 1);
+			return 1;
+		}
+
+		[MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+		public static int panic(IntPtr L)
+		{
+			string reason = String.Format("unprotected error in call to Lua API ({0})", LuaDLL.lua_tostring(L, -1));
+			throw new LuaException(reason);
+		}
+		
+		[MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+		public static int error_traceback(IntPtr L)
+		{
+			LuaDLL.lua_getglobal(L,"debug");
+			LuaDLL.lua_getfield(L,-1,"traceback");
+			LuaDLL.lua_pushvalue(L,1);
+			LuaDLL.lua_pushnumber(L,2);
+			LuaDLL.lua_call (L,2,1);
+			return 1;
+		}
+		
+		[MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+		public static int print(IntPtr L)
+		{
+			// For each argument we'll 'tostring' it
+			int n = LuaDLL.lua_gettop(L);
+			string s = String.Empty;
+			
+			LuaDLL.lua_getglobal(L, "tostring");
+			
+			for( int i = 1; i <= n; i++ ) 
+			{
+				LuaDLL.lua_pushvalue(L, -1);  /* function to be called */
+				LuaDLL.lua_pushvalue(L, i);   /* value to print */
+				LuaDLL.lua_call(L, 1, 1);
+				s += LuaDLL.lua_tostring(L, -1);
+				
+				if( i > 1 ) 
+				{
+					s += "\t";
+				}
+				
+				LuaDLL.lua_pop(L, 1);  /* pop result */
+				
+				Debug.Log("LUA: " + s);
+			}
+			return 0;
+		}
+		
+		[MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+		public static int loader(IntPtr L)
+		{
+			// Get script to load
+			string fileName = String.Empty;
+			fileName = LuaDLL.lua_tostring(L, 1);
+			fileName = fileName.Replace('.', '/');
+			fileName += ".lua";
+			
+			// Load with Unity3D resources
+			TextAsset file = (TextAsset)Resources.Load(fileName);
+			if( file == null )
+			{
+				return 0;
+			}
+			
+			LuaDLL.luaL_loadbuffer(L, file.text, Encoding.UTF8.GetByteCount(file.text), fileName);
+			
+			return 1;
+		}
+		
+		[MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+		public static int dofile(IntPtr L)
+		{
+			// Get script to load
+			string fileName = String.Empty;
+			fileName = LuaDLL.lua_tostring(L, 1);
+			fileName.Replace('.', '/');
+			fileName += ".lua";
+			
+			int n = LuaDLL.lua_gettop(L);
+			
+			// Load with Unity3D resources
+			TextAsset file = (TextAsset)Resources.Load(fileName);
+			if( file == null )
+			{
+				return LuaDLL.lua_gettop(L) - n;
+			}
+			
+			if( LuaDLL.luaL_loadbuffer(L, file.text, Encoding.UTF8.GetByteCount(file.text), fileName) == 0 )
+			{
+				LuaDLL.lua_call(L, 0, LuaDLL.LUA_MULTRET);
+			}
+			
+			return LuaDLL.lua_gettop(L) - n;
+		}
+		
+		[MonoPInvokeCallbackAttribute(typeof(LuaCSFunction))]
+		public static int loadfile(IntPtr L)
+		{
+			return loader(L);
+		}
+		
+		public static string init_luanet =
+			@"local metatable = {}
+            local rawget = rawget
+            local import_type = luanet.import_type
+            local load_assembly = luanet.load_assembly
+            luanet.error, luanet.type = error, type
+            -- Lookup a .NET identifier component.
+            function metatable:__index(key) -- key is e.g. 'Form'
+            -- Get the fully-qualified name, e.g. 'System.Windows.Forms.Form'
+            local fqn = rawget(self,'.fqn')
+            fqn = ((fqn and fqn .. '.') or '') .. key
+
+            -- Try to find either a luanet function or a CLR type
+            local obj = rawget(luanet,key) or import_type(fqn)
+
+            -- If key is neither a luanet function or a CLR type, then it is simply
+            -- an identifier component.
+            if obj == nil then
+                -- It might be an assembly, so we load it too.
+                    pcall(load_assembly,fqn)
+                    obj = { ['.fqn'] = fqn }
+            setmetatable(obj, metatable)
+            end
+
+            -- Cache this lookup
+            rawset(self, key, obj)
+            return obj
+            end
+
+            -- A non-type has been called; e.g. foo = System.Foo()
+            function metatable:__call(...)
+            error('No such type: ' .. rawget(self,'.fqn'), 2)
+            end
+
+            -- This is the root of the .NET namespace
+            luanet['.fqn'] = false
+            setmetatable(luanet, metatable)
+
+            -- Preload the mscorlib assembly
+            luanet.load_assembly('mscorlib')";
+	}
+}
